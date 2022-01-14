741e9322f8f94415a70daa3712bcb7e456075e1d
Sashmitha Ravindu <sashmitharavindu33@gmail.com>
2022-01-14 19:10:34 +0530

    Complete UI

--- src/controller/IDEMainDashboardFormController.java
+++ src/controller/IDEMainDashboardFormController.java
@@ -1,4 +1,79 @@
 package controller;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.*;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
+
 public class IDEMainDashboardFormController {
+    public AnchorPane mainContext;
+    public SplitMenuButton menuFile;
+    public MenuItem optionNew;
+    public MenuItem optionOpen;
+    public MenuItem optionSave;
+    public MenuItem optionPrint;
+    public MenuItem optionFind;
+    public MenuItem optionReplace;
+    public MenuItem optionExit;
+    public SplitMenuButton menuEdit;
+    public MenuItem optionCut;
+    public MenuItem optionCopy;
+    public MenuItem optionPaste;
+    public MenuItem optionSelectAll;
+    public SplitMenuButton menuHelp;
+    public MenuItem optionAboutUs;
+    public Button btnNewFile;
+    public ImageView imgNewFile;
+    public Button btnSave;
+    public ImageView imgSave;
+    public Button btnCopy;
+    public ImageView imgCopy;
+    public Button btnCut;
+    public ImageView imgCut;
+    public Button btnPaste;
+    public ImageView imgPaste;
+    public Button btnSelectAll;
+    public ImageView imgSelectAll;
+    public TextField txtFind;
+    public Button btnFind;
+    public TextField txtReplace;
+    public Button btnReplaceAll;
+    public Button btnReplace;
+    public BorderPane wordsCountPane;
+    public Label lblWordsCount;
+    public BorderPane findsCountPane;
+    public Label lblFindsCount;
+    public TextArea txtArea;
+
+    public void initialize(){
+
+    }
+
+    public void btnReplace_OnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnReplaceAll_OnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnFind_OnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnNewFile_OnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnSave_OnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnCopy_OnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnCut_OnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnPaste_OnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnSelectAll_OnAction(ActionEvent actionEvent) {
+    }
 }
